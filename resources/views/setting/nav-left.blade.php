e34a4af1a14d8c079abd412924c78da626dd8672
fajar.iman@cartenz.co.id <fajarimansuryadi@gmail.com>
2019-02-01 09:53:56 +0700

    create handling profile user, black white list user, and login with is blacklist user

--- resources/views/setting/nav-left.blade.php
+++ resources/views/setting/nav-left.blade.php
@@ -25,11 +25,13 @@
               <h4><i class="fa fa-users"></i>&nbsp;&nbsp;<span>Users</span></h4>
             </a>
           </li>
-          <li class="">
-            <a href="{{route('setting.blackwhitelist')}}">
-              <h4><i class="fa fa-ban"></i>&nbsp;<span>Blacklist and Whitelist</span></h4>
-            </a>
-          </li>
+          @if ($user_role[0]->role == '1-Super Admin' or $user_role[0]->role == '2-Admin')
+            <li class="">
+              <a href="{{route('setting.blackwhitelist')}}">
+                <h4><i class="fa fa-ban"></i>&nbsp;<span>Blacklist and Whitelist</span></h4>
+              </a>
+            </li>
+          @endif
           <li class="footer">
             <div class="footer-tools">
               <a href="#">
