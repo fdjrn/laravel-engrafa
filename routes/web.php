1696b75b3566a5aa8ea07cda42bbb0a2fd84978b
arissunjaya <sunjayaaris@gmail.com>
2019-02-17 22:03:45 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -58,19 +58,6 @@ Route::middleware(['auth','web'])->group(function () {
     Route::get('/index','Index\IndexController@index')->name('index');
     Route::post('/index','Index\IndexController@index')->name('index.last.folder');
     Route::get('/index/list-all/{id}','Index\IndexController@getListAll');
-
-
-	// Route::post('/ajax_get_dashboard', 'Dashboard\DashboardController@ajax_get_dashboard'); 
-	// Route::get('/ajax/edit-survey/{id}', 'Dashboard\DashboardController@ajax_edit_survey');
-	
-
-	// index & file explorer
-	Route::get('/index','Index\IndexController@index')->name('index');
-	Route::get('/index/detail','Index\IndexDetailController@index')->name('index.detail');
-    Route::get('/index/list-all','Index\IndexController@getListAll');
-    // Route::get('/index/list-all/{id}','Index\IndexController@getListDetail');
-
-
     Route::get('/index/list-all-previous/{id}','Index\IndexController@getListAllPrevious');
     Route::get('/index/list-folder/{id}','Index\IndexController@getListAllFolder');
     Route::get('/index/list-folder-previous/{id}','Index\IndexController@getListPreviousFolder');
