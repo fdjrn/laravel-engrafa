920dcc5dc7c05ac995e75921c3639240d5bfb297
fajar.iman@cartenz.co.id <fajarimansuryadi@gmail.com>
2019-02-18 23:06:50 +0700

    hapus teks @toastr_css

--- resources/views/layouts/part/styles.blade.php
+++ resources/views/layouts/part/styles.blade.php
@@ -54,7 +54,7 @@
   <!-- Theme style -->
   <link rel="stylesheet" href=" {{ asset('theme/AdminLTE/plugins/pace/pace.min.css')}}">
   <link rel="stylesheet" href=" {{ asset('theme/AdminLTE/dist/css/AdminLTE.min.css')}}">
-  
+  @toastr_css
   <link rel="stylesheet" href=" {{ asset('theme/AdminLTE/bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css')}}">
   {{--<link rel="stylesheet" href=" {{ asset('theme/AdminLTE/plugins/sweetalert/sweetalert.css')}}">--}}
   {{ Html::style('css/sweetalert2.min.css')}}
